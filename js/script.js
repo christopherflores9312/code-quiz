af0690875266a3432f0e6ed4c3f7c0d38cfde586
christopherflores9312 <christopherflores9312@gmail.com>
2023-03-09 16:41:40 -0700

    starter functions added

---
+++ js/script.js
@@ -0,0 +1,35 @@
+function startQuiz() {
+    // start the timer
+    // present the first question
+  }
+
+  
+  function presentQuestion() {
+    // present a question
+    // wait for an answer
+    // check if the answer is correct
+    // if correct, present the next question
+    // if incorrect, subtract time from the timer and present the next question
+  }
+
+  
+  function checkAnswer(answer) {
+    // check if the answer is correct
+    // if correct, return true
+    // if incorrect, return false
+  }
+  
+  function subtractTime() {
+    // subtract time from the timer
+  }
+  
+  function endGame() {
+    // stop the timer
+    // present the score
+    // ask the user to save their initials and score
+  }
+  
+  function saveScore(initials, score) {
+    // save the initials and score
+  }
+  

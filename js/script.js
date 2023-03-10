7175debfde63cca021f1c2628479d3827c4aba28
christopherflores9312 <christopherflores9312@gmail.com>
2023-03-10 09:29:37 -0700

    define elements

--- js/script.js
+++ js/script.js
@@ -1,35 +1,41 @@
+// select elements from the DOM
+const startButton = document.querySelector('#start-button');
+const quizSection = document.querySelector('#quiz');
+const gameOverSection = document.querySelector('#game-over');
+const scoreDisplay = document.querySelector('#score');
+const initialsInput = document.querySelector('#initials');
+const timerDisplay = document.querySelector('#timer');
+
 function startQuiz() {
     // start the timer
     // present the first question
-  }
+}
+  
+function presentQuestion() {
+// present a question
+// wait for an answer
+// check if the answer is correct
+// if correct, present the next question
+// if incorrect, subtract time from the timer and present the next question
+}
 
-  
-  function presentQuestion() {
-    // present a question
-    // wait for an answer
-    // check if the answer is correct
-    // if correct, present the next question
-    // if incorrect, subtract time from the timer and present the next question
-  }
+function checkAnswer(answer) {
+// check if the answer is correct
+// if correct, return true
+// if incorrect, return false
+}
 
-  
-  function checkAnswer(answer) {
-    // check if the answer is correct
-    // if correct, return true
-    // if incorrect, return false
-  }
-  
-  function subtractTime() {
-    // subtract time from the timer
-  }
-  
-  function endGame() {
-    // stop the timer
-    // present the score
-    // ask the user to save their initials and score
-  }
-  
-  function saveScore(initials, score) {
-    // save the initials and score
-  }
+function subtractTime() {
+// subtract time from the timer
+}
+
+function endGame() {
+// stop the timer
+// present the score
+// ask the user to save their initials and score
+}
+
+function saveScore(initials, score) {
+// save the initials and score
+}
   

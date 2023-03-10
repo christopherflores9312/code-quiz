c2e2443cf3a5ae658119367a6a030d07875825b6
christopherflores9312 <christopherflores9312@gmail.com>
2023-03-10 10:10:57 -0700

    added questions object

--- js/script.js
+++ js/script.js
@@ -14,6 +14,35 @@ let timer = 60; // in seconds
 let currentQuestionIndex = 0;
 let score = 0;
 
+// define the quiz questions and answers
+const questions = [
+    {
+      question: 'What is CSS?',
+      answers: ['Cascading Style Syntax', 'Cascading System Syntax', 'Cascading Style Sheet', 'Computer Style Syntax'],
+      correctAnswerIndex: 2
+    },
+    {
+      question: 'Which of the following is not a valid way to declare a JavaScript variable?',
+      answers: ['let myVar = 42;', 'var myVar = 42;', 'const myVar = 42;', 'myVar = 42;'],
+      correctAnswerIndex: 3
+    },
+    {
+      question: 'Which of the following is a valid way to create a new object in JavaScript?',
+      answers: ['let obj = {}', 'let obj = new Object()', 'let obj = Object.create()', 'All of the above'],
+      correctAnswerIndex: 3
+    },
+    {
+      question: 'Which of the following is a valid way to add a new element to the end of an array in JavaScript?',
+      answers: ['myArray.push(newElement)', 'myArray.unshift(newElement)', 'myArray.concat(newElement)', 'myArray.splice(myArray.length, 0, newElement)'],
+      correctAnswerIndex: 0
+    },
+    {
+      question: 'What is the difference between let and var in JavaScript?',
+      answers: ['let is used for block-scoped variables, while var is used for function-scoped variables.', 'let is used for function-scoped variables, while var is used for block-scoped variables.', 'There is no difference between let and var.', 'let is a reserved keyword in JavaScript, while var is not.'],
+      correctAnswerIndex: 0
+    }
+  ];
+
 function startQuiz() {
     // start the timer
     // present the first question
